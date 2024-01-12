6cf07912c2ae39a601bc0355c3cca99f3b468ff1
Ali23ha <ah7690728@gmail.com>
2024-01-12 04:00:53 -0800

    recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - the length
+ * @s: pointer to string params
+ * Return: recursion
+ * author: ali_hasan
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
