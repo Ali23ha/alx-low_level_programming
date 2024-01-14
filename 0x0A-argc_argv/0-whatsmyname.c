fb237c68ce1f1e05c43c25fa09b2fb21eb3aad1a
Ali23ha <ah7690728@gmail.com>
2024-01-14 12:27:38 -0800

    argc & argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc __attribute__((unused)),char *argv[])
+{
+    char *arg1 = argv[0];
+    printf("%s \n", arg1);
+   
+    return 0;
+}
+
